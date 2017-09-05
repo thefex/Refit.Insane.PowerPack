b2cfc083d62f2a10e15a13a28ead78354f8fe417
Jakub Kaprzyk <jakub.kaprzyk@solidbrain.com>
2017-09-05 09:39:08 +0200

    Added posibility to add api timeout

--- Refit.Insane.PowerPack/Attributes/ApiDefinitionAttribute.cs
+++ Refit.Insane.PowerPack/Attributes/ApiDefinitionAttribute.cs
@@ -4,21 +4,33 @@ namespace Refit.Insane.PowerPack.Attributes
 {
     public class ApiDefinitionAttribute : Attribute
     {
-        public ApiDefinitionAttribute(string baseUri) : this()
+        public ApiDefinitionAttribute(string baseUri) : this(5)
         {
             BaseUri = baseUri;
             HttpClientHandlerType = typeof(HttpClientDiagnosticsHandler);
         }
         
-        public ApiDefinitionAttribute(string baseUri, Type httpClientHandlerType) : this()
+        public ApiDefinitionAttribute(string baseUri, int apiTimoutInSeconds) : this(apiTimoutInSeconds)
+        {
+            BaseUri = baseUri;
+            HttpClientHandlerType = typeof(HttpClientDiagnosticsHandler);
+        }
+        
+        public ApiDefinitionAttribute(string baseUri, int apiTimeoutInSeconds, Type httpClientHandlerType) : this(apiTimeoutInSeconds)
+        {
+            BaseUri = baseUri;
+            HttpClientHandlerType = httpClientHandlerType;
+        }
+        
+        public ApiDefinitionAttribute(string baseUri, Type httpClientHandlerType) : this(5)
         {
             BaseUri = baseUri;
             HttpClientHandlerType = httpClientHandlerType;
         }
 
-        private ApiDefinitionAttribute()
+        private ApiDefinitionAttribute(int apiTimeoutInSeconds)
         {
-            ApiTimeout = TimeSpan.FromSeconds(5);
+            ApiTimeout = TimeSpan.FromSeconds(apiTimeoutInSeconds);
         }
 
         public TimeSpan ApiTimeout { get; }
