b56f238358caa813c8b17afc2d7ea5b232543714
Adrien Padol <apadol@ikomobi.com>
2018-12-04 10:23:19 +0100

    Let ApiDefinitionAttribute be used to specify a custom HttpClientHandler

--- Refit.Insane.PowerPack/Attributes/ApiDefinitionAttribute.cs
+++ Refit.Insane.PowerPack/Attributes/ApiDefinitionAttribute.cs
@@ -5,24 +5,29 @@ namespace Refit.Insane.PowerPack.Attributes
     public class ApiDefinitionAttribute : Attribute
     {
         private const int DefaultTimeoutInSeconds = 5;
-        
+
+        public ApiDefinitionAttribute(Type httpClientHandlerType) : this(DefaultTimeoutInSeconds)
+        {
+            HttpClientHandlerType = httpClientHandlerType;
+        }
+
         public ApiDefinitionAttribute(string baseUri) : this(DefaultTimeoutInSeconds)
         {
             BaseUri = baseUri;
             HttpClientHandlerType = typeof(HttpClientDiagnosticsHandler);
         }
-        
+
         public ApiDefinitionAttribute(string baseUri, int apiTimoutInSeconds) : this(apiTimoutInSeconds)
         {
             BaseUri = baseUri;
             HttpClientHandlerType = typeof(HttpClientDiagnosticsHandler);
         }
-        
+
         public ApiDefinitionAttribute(string baseUri, Type httpClientHandlerType) : this(baseUri)
         {
             HttpClientHandlerType = httpClientHandlerType;
         }
-        
+
         public ApiDefinitionAttribute(string baseUri, int apiTimeoutInSeconds, Type httpClientHandlerType) : this(baseUri, apiTimeoutInSeconds)
         {
             HttpClientHandlerType = httpClientHandlerType;
@@ -36,7 +41,7 @@ namespace Refit.Insane.PowerPack.Attributes
         public TimeSpan ApiTimeout { get; }
 
         public string BaseUri { get; }
-        
+
         public Type HttpClientHandlerType { get; }
     }
 }
